027f72e11486df2f4744cbd7700b2b86c9b5c3aa
Raunak Raj <71929976+bajrangCoder@users.noreply.github.com>
2025-05-14 16:29:01 +0530

    fix: acode javascript console issue

--- src/lib/run.js
+++ src/lib/run.js
@@ -1,3 +1,4 @@
+import ajax from "@deadlyjack/ajax";
 import tutorial from "components/tutorial";
 import alert from "dialogs/alert";
 import box from "dialogs/box";
@@ -133,7 +134,7 @@ async function run(
 
 		//this extra www is incorrect because asset_directory itself has www
 		//but keeping it in case something depends on it
-		pathName = `${ASSETS_DIRECTORY}www/`;
+		pathName = "/";
 		port = constants.CONSOLE_PORT;
 
 		start();
@@ -191,9 +192,9 @@ async function run(
 					isConsole ||
 					appSettings.value.console === appSettings.CONSOLE_LEGACY
 				) {
-					url = `${ASSETS_DIRECTORY}/js/build/console.build.js`;
+					url = constants.CONSOLE_BUILD_PATH;
 				} else {
-					url = `${DATA_STORAGE}/eruda.js`;
+					url = `${DATA_STORAGE}eruda.js`;
 				}
 				sendFileContent(url, reqId, "application/javascript");
 				break;
@@ -478,14 +479,22 @@ async function run(
 	 * @returns
 	 */
 	async function sendFileContent(url, id, mime, processText) {
-		const fs = fsOperation(url);
+		let text;
+		if (url.includes(constants.CONSOLE_BUILD_PATH)) {
+			text = await ajax.get(url, {
+				responseType: "text",
+			});
+		} else {
+			const fs = fsOperation(url);
 
-		if (!(await fs.exists())) {
-			error(id);
-			return;
+			if (!(await fs.exists())) {
+				error(id);
+				return;
+			}
+
+			text = await fs.readFile(appSettings.value.defaultFileEncoding);
 		}
 
-		let text = await fs.readFile(appSettings.value.defaultFileEncoding);
 		text = processText ? processText(text) : text;
 		if (mime === MIMETYPE_HTML) {
 			sendHTML(text, id);
