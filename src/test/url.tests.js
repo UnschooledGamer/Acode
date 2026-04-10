f14a1ce607a2ae58e5865cb7ac66b41f7b0b07c1
Raunak Raj <71929976+bajrangCoder@users.noreply.github.com>
2026-04-10 22:59:13 +0530

    fix: tests

--- src/test/url.tests.js
+++ src/test/url.tests.js
@@ -1,135 +1,95 @@
 import Url from "../utils/Url";
 import { TestRunner } from "./tester";
 
+const JOIN_CASES = [
+	{
+		name: "Android SAF join",
+		folderUrl:
+			"content://com.android.externalstorage.documents/tree/primary%3ATesthtml",
+		activeLocation:
+			"content://com.android.externalstorage.documents/tree/primary%3ATesthtml::primary:Testhtml/Styles/",
+		expectedJoined:
+			"content://com.android.externalstorage.documents/tree/primary%3ATesthtml::primary:Testhtml/Styles/index.html",
+	},
+	{
+		name: "Termux SAF join",
+		folderUrl:
+			"content://com.termux.documents/tree/%2Fdata%2Fdata%2Fcom.termux%2Ffiles%2Fhome%2Facode-site-ui",
+		activeLocation:
+			"content://com.termux.documents/tree/%2Fdata%2Fdata%2Fcom.termux%2Ffiles%2Fhome%2Facode-site-ui::/data/data/com.termux/files/home/acode-site-ui/",
+		expectedJoined:
+			"content://com.termux.documents/tree/%2Fdata%2Fdata%2Fcom.termux%2Ffiles%2Fhome%2Facode-site-ui::/data/data/com.termux/files/home/acode-site-ui/index.html",
+	},
+	{
+		name: "Acode SAF join",
+		folderUrl:
+			"content://com.foxdebug.acode.documents/tree/%2Fdata%2Fuser%2F0%2Fcom.foxdebug.acode%2Ffiles%2Fpublic",
+		activeLocation:
+			"content://com.foxdebug.acode.documents/tree/%2Fdata%2Fuser%2F0%2Fcom.foxdebug.acode%2Ffiles%2Fpublic::/data/user/0/com.foxdebug.acode/files/public/",
+		expectedJoined:
+			"content://com.foxdebug.acode.documents/tree/%2Fdata%2Fuser%2F0%2Fcom.foxdebug.acode%2Ffiles%2Fpublic::/data/user/0/com.foxdebug.acode/files/public/index.html",
+	},
+];
+
+const TRAILING_SLASH_CASES = [
+	{
+		name: "Android SAF trailing slash",
+		a: "content://com.android.externalstorage.documents/tree/primary%3ATesthtml/",
+		b: "content://com.android.externalstorage.documents/tree/primary%3ATesthtml",
+	},
+	{
+		name: "Termux SAF trailing slash",
+		a: "content://com.termux.documents/tree/%2Fdata%2Fdata%2Fcom.termux%2Ffiles%2Fhome%2Facode-site-ui/",
+		b: "content://com.termux.documents/tree/%2Fdata%2Fdata%2Fcom.termux%2Ffiles%2Fhome%2Facode-site-ui",
+	},
+	{
+		name: "Acode SAF trailing slash",
+		a: "content://com.foxdebug.acode.documents/tree/%2Fdata%2Fuser%2F0%2Fcom.foxdebug.acode%2Ffiles%2Fpublic/",
+		b: "content://com.foxdebug.acode.documents/tree/%2Fdata%2Fuser%2F0%2Fcom.foxdebug.acode%2Ffiles%2Fpublic",
+	},
+];
+
+function assertJoinCase(
+	test,
+	{ folderUrl, activeLocation, expectedJoined, segment },
+) {
+	const joined = Url.join(activeLocation, segment || "index.html");
+
+	test.assert(joined !== null, "Joining the SAF URL should return a value");
+	test.assertEqual(
+		joined,
+		expectedJoined,
+		"Joined URL should match the expected SAF file URI",
+	);
+	test.assert(
+		!Url.areSame(folderUrl, joined),
+		"Folder URL and joined file URL should not be considered the same",
+	);
+}
+
 export async function runUrlTests(writeOutput) {
-	const runner = new TestRunner("URL / SAF URI Tests");
+	const runner = new TestRunner("URL / SAF URIs");
 
-	runner.test(
-		"Android external storage: join active location + index.html",
-		(test) => {
-			const folderUrl =
-				"content://com.android.externalstorage.documents/tree/primary%3ATesthtml";
-			const activeLocation =
-				"content://com.android.externalstorage.documents/tree/primary%3ATesthtml::primary:Testhtml/Styles/";
-			const expectedJoined =
-				"content://com.android.externalstorage.documents/tree/primary%3ATesthtml::primary:Testhtml/Styles/index.html";
+	for (const joinCase of JOIN_CASES) {
+		runner.test(joinCase.name, (test) => {
+			assertJoinCase(test, joinCase);
+		});
+	}
 
-			const joined = Url.join(activeLocation, "index.html");
-
-			test.assertEqual(
-				joined,
-				expectedJoined,
-				"Joined URL should match expected Android SAF file URI",
-			);
+	for (const trailingSlashCase of TRAILING_SLASH_CASES) {
+		runner.test(trailingSlashCase.name, (test) => {
 			test.assert(
-				!Url.areSame(folderUrl, joined),
-				"Folder URL and joined file URL should not be considered same",
+				Url.areSame(trailingSlashCase.a, trailingSlashCase.b),
+				"Folder URLs differing only by a trailing slash should be same",
 			);
-		},
-	);
+		});
+	}
 
-	runner.test("Termux SAF: join active location + index.html", (test) => {
-		const folderUrl =
-			"content://com.termux.documents/tree/%2Fdata%2Fdata%2Fcom.termux%2Ffiles%2Fhome%2Facode-site-ui";
-		const activeLocation =
-			"content://com.termux.documents/tree/%2Fdata%2Fdata%2Fcom.termux%2Ffiles%2Fhome%2Facode-site-ui::/data/data/com.termux/files/home/acode-site-ui/";
-		const expectedJoined =
-			"content://com.termux.documents/tree/%2Fdata%2Fdata%2Fcom.termux%2Ffiles%2Fhome%2Facode-site-ui::/data/data/com.termux/files/home/acode-site-ui/index.html";
-
-		const joined = Url.join(activeLocation, "index.html");
-
-		test.assertEqual(
-			joined,
-			expectedJoined,
-			"Joined URL should match expected Termux SAF file URI",
-		);
-		test.assert(
-			!Url.areSame(folderUrl, joined),
-			"Folder URL and joined file URL should not be considered same",
-		);
-	});
-
-	runner.test(
-		"Acode terminal SAF: join active location + index.html",
-		(test) => {
-			const folderUrl =
-				"content://com.foxdebug.acode.documents/tree/%2Fdata%2Fuser%2F0%2Fcom.foxdebug.acode%2Ffiles%2Fpublic";
-			const activeLocation =
-				"content://com.foxdebug.acode.documents/tree/%2Fdata%2Fuser%2F0%2Fcom.foxdebug.acode%2Ffiles%2Fpublic::/data/user/0/com.foxdebug.acode/files/public/";
-			const expectedJoined =
-				"content://com.foxdebug.acode.documents/tree/%2Fdata%2Fuser%2F0%2Fcom.foxdebug.acode%2Ffiles%2Fpublic::/data/user/0/com.foxdebug.acode/files/public/index.html";
-
-			const joined = Url.join(activeLocation, "index.html");
-
-			test.assertEqual(
-				joined,
-				expectedJoined,
-				"Joined URL should match expected Acode Terminal SAF file URI",
-			);
-			test.assert(
-				!Url.areSame(folderUrl, joined),
-				"Folder URL and joined file URL should not be considered same",
-			);
-		},
-	);
-
-	runner.test(
-		"Android SAF folder URL should match with trailing slash",
-		(test) => {
-			const a =
-				"content://com.android.externalstorage.documents/tree/primary%3ATesthtml/";
-			const b =
-				"content://com.android.externalstorage.documents/tree/primary%3ATesthtml";
-
-			test.assert(
-				Url.areSame(a, b),
-				"Android folder URLs differing only by trailing slash should be same",
-			);
-		},
-	);
-
-	runner.test(
-		"Termux SAF folder URL should match with trailing slash",
-		(test) => {
-			const a =
-				"content://com.termux.documents/tree/%2Fdata%2Fdata%2Fcom.termux%2Ffiles%2Fhome%2Facode-site-ui/";
-			const b =
-				"content://com.termux.documents/tree/%2Fdata%2Fdata%2Fcom.termux%2Ffiles%2Fhome%2Facode-site-ui";
-
-			test.assert(
-				Url.areSame(a, b),
-				"Termux folder URLs differing only by trailing slash should be same",
-			);
-		},
-	);
-
-	runner.test(
-		"Acode terminal SAF folder URL should match with trailing slash",
-		(test) => {
-			const a =
-				"content://com.foxdebug.acode.documents/tree/%2Fdata%2Fuser%2F0%2Fcom.foxdebug.acode%2Ffiles%2Fpublic/";
-			const b =
-				"content://com.foxdebug.acode.documents/tree/%2Fdata%2Fuser%2F0%2Fcom.foxdebug.acode%2Ffiles%2Fpublic";
-
-			test.assert(
-				Url.areSame(a, b),
-				"Acode terminal folder URLs differing only by trailing slash should be same",
-			);
-		},
-	);
-
-	runner.test("join should handle leading slash segment", (test) => {
-		const activeLocation =
-			"content://com.android.externalstorage.documents/tree/primary%3ATesthtml::primary:Testhtml/Styles/";
-		const expectedJoined =
-			"content://com.android.externalstorage.documents/tree/primary%3ATesthtml::primary:Testhtml/Styles/index.html";
-
-		const joined = Url.join(activeLocation, "/index.html");
-		test.assertEqual(
-			joined,
-			expectedJoined,
-			"Leading slash in joined segment should be normalized",
-		);
+	runner.test("Android SAF leading slash", (test) => {
+		assertJoinCase(test, {
+			...JOIN_CASES[0],
+			segment: "/index.html",
+		});
 	});
 
 	return await runner.run(writeOutput);
